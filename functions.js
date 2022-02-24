f29ff73bcac5c7c749d35540f24160701eb1d774
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-02-24 14:52:33 -0600

    finished name function

---
+++ functions.js
@@ -0,0 +1,29 @@
+//You see repetition, you make a function
+// const movie1 = 'Saving Private Ryan';
+// const year1 = 1998;
+// console.log(`${movie1} was released in ${year1}`);
+
+// const movie2 = 'Interstellar';
+// const year2 = 2014;
+// console.log(`${movie2} was released in ${year2}`);
+
+// const movie3 = 'Jason Bourne';
+// const year3 = 2016;
+// console.log(`${movie3} was released in ${year3}`);
+
+// const printMovie = (movie, year) => {
+// console.log(`${movie} was released in ${year}`)
+// }
+
+// printMovie('Saving Private Ryan', 1998);
+// printMovie('Interstellar', 2014);
+// printMovie('Jason Bourne', 2016);
+
+// how to construct an arrow function
+
+const printName = (name) => {
+    console.log(`I am ${name}!!`)
+}
+
+printName('Spartacus');
+
