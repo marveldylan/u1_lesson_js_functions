3e7685e8e4c318db0e2884cd23d65b3c1c2ea60b
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-02-24 15:30:04 -0600

    exercises 1 - 7 complete

--- functions.js
+++ functions.js
@@ -21,9 +21,105 @@
 
 // how to construct an arrow function
 
-const printName = (name) => {
-    console.log(`I am ${name}!!`)
+// const printName = (name) => {
+//     console.log(`I am ${name}!!`)
+// }
+
+// printName('Spartacus');
+
+// const calcArea = (x, y) => {
+//     let area = x * y;
+//     console.log(`The area of a ${x} u x ${y} u rectangle is ${area} square units.`);
+// }
+
+// calcArea(25, 50);
+
+// Exercise 1
+
+const logHello = () => {
+    console.log(`Hello World`)
 }
 
-printName('Spartacus');
+//logHello();
 
+// Exercise 2
+const sayWhatever = (input) => {
+    console.log(`I made the computer say: ${input}`)
+}
+// sayWhatever('Wazzup!')
+// Exercise 3
+const printNums = () => {
+    for(let i = 1; i <= 100; i++) {
+        console.log(i);
+    }
+}
+// printNums();
+// Exercise 4
+
+myArr = [1, 2, 3, 4, 5];
+sum = 0;
+
+const  sumArr = (myArray) =>  {
+    for(let i = 0; i < myArray.length; i++) {
+        sum = sum + myArray[i];
+    }
+    console.log(`The sum of the array is ${sum}`);
+}
+
+// sumArr(myArr);
+
+// Exercise 5
+const students = [
+    'Alice',
+    'Andrew',
+    'Casey',
+    'Damian',
+    'Grant',
+    'Howie',
+    'Wade',
+    'Kat',
+    'Kimbrad',
+    'Kiu',
+    'Natasha',
+    'Obi',
+    'Pedro',
+    'Sarah',
+    'Scott',
+    'Tiffany',
+    'Zhe',
+  ];
+
+  const printStudents = (myArray) => {
+      for(i = 0; i < myArray.length; i++) {
+          console.log(myArray[i])
+      }
+  }
+
+  //printStudents(students);
+
+  // Exercise 6
+array6 = ["test", "it", "baby", 'yeahhhhh'];
+
+const arrayLength = (myArray) => {
+    let returnArray = [];
+    for(let i = 0; i < myArray.length; i++){
+        let itemLength = myArray[i].length
+        returnArray.push(itemLength);
+    }
+    console.log(returnArray)
+}
+
+// arrayLength(array6);
+  // Exercise 7
+
+let productPower= (x, y, z) => {
+    let prod = (x * y)
+    let power = Math.pow(prod, z)
+    console.log(`The answer is ${power}`)
+}
+
+//productPower(2,3,4);
+  // Bonus Exercises
+  // 1
+
+  // 2
